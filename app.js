fcc134cdf405201cca59b7227f05cc6fb2838047
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-08-01 21:43:09 -0400

    Allow enviroments to set own ports

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const port = 3000;
 app.use(express.static('views'));
 app.use(express.static('public'));
 
+// Create and start the server
+const port = process.env.PORT || '3000'; 
 app.listen(port, () => {
-    console.log("Listening on port",port);
+    console.log(`Listening on localhost:${port}`);
 });
