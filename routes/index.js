33e4a06ee45f07528342dee8ee36bf0f00a28f7a
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-08-02 13:15:02 -0400

    Fix routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const timestamp = require('../api/timestamp.js');
 
 // Handle routes
-router.route('/timestamp/api/:query').get(timestamp.parser);
+router.route('/api/timestamp/').get(timestamp.parser);
+router.route('/api/timestamp/:query').get(timestamp.parser);
 
 // Main page
 router.get('/', (request, response) => {
